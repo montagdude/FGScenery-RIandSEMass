3136fc45cf53e0fa88c121d1633e0f73089bb45c
Daniel Prosser <dpross1100@msn.com>
2020-02-28 23:57:03 -0500

    Update scripts to get North boundary right

--- process_airports.sh
+++ process_airports.sh
@@ -2,4 +2,4 @@
 
 # I have apt.dat at ~/Projects/FGScenery_tools/Airports/apt.dat (copy it here before
 # running to make things simpler for docker)
-genapts850 --input=apt.dat --work=./work --dem-path=SRTM-3 --min-lat=41.0 --max-lat=42.15 --min-lon=-72 --max-lon=-69.5
+genapts850 --input=apt.dat --work=./work --dem-path=SRTM-3 --min-lat=41.0 --max-lat=42.125 --min-lon=-72 --max-lon=-69.5
--- tg-construct.sh
+++ tg-construct.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-function construct ()
-{
-    local WBND=$1
-    local EBND=$2
-    local SBND=$3
-    local NBND=$4
-
-    # Note: Ocean landclass is not needed, as that will be the default when nothing else is
-    # there. ogr-decode won't even write any output data for Ocean shapefiles.
-    tg-construct --ignore-landmass --work-dir=./work --output-dir=./output/Terrain --min-lon=$WBND --max-lon=$EBND --min-lat=$SBND --max-lat=$NBND AirportArea AirportObj Bog DeciduousForest DryCrop EvergreenForest Grassland Greenspace IrrCrop Lake Marsh MixedForest Rock SRTM-3 Sand Scrub Stream SubUrban Town Urban
-}
-
 # Total extents
 WEST=${WEST:--72}
-EAST=${EAST:--69.6}
+EAST=${EAST:--69.5}
 SOUTH=${SOUTH:-41}
-NORTH=${NORTH:-42.15}
+NORTH=${NORTH:-41.9}    # This results in scenery extending to 42.125. Don't know
+                        # why. It was found through trial and error.
 
-construct $WEST $EAST $SOUTH $NORTH
+# Note: Ocean landclass is not needed, as that will be the default when nothing else is
+# there. ogr-decode won't even write any output data for Ocean shapefiles.
+tg-construct --ignore-landmass --work-dir=./work --output-dir=./output/Terrain --min-lon=$WEST --max-lon=$EAST --min-lat=$SOUTH --max-lat=$NORTH AirportArea AirportObj Bog DeciduousForest DryCrop EvergreenForest Grassland Greenspace IrrCrop Lake Marsh MixedForest Rock SRTM-3 Sand Scrub Stream SubUrban Town Urban
+
